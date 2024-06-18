7dbb4f46bfa0ee1b71d0243e6d98d9871b489205
unknown <adham111mahfoud@gmail.com>
2024-06-18 19:47:26 +0300

    upload the project

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'IMDB',
+  description: 'Welcome To IMDB Movies and Series DataBase, Here you can find all the movies and series you need', 
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
